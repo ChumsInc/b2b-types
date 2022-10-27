22e41ac7972b33f0264c110b10a8776f12bc85d4
Steve Montgomery <steve@chums.com>
2022-10-27 12:04:07 -0600

    fix: export invoice & so types;

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 export * from './b2b-error';
 export * from './customer';
 export * from './generic';
+export * from './invoice';
 export * from './keyword';
 export * from './products';
 export * from './promo-code';
+export * from './sales-order';
 export * from './ui-features';
 export * from './user';
 
 export * from './product-constants'
 export * from './product-utils'
+
