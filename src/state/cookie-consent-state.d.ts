d59b42f4bb69f1e8f686657680f015ef05b24a65
Steve Montgomery <steve@chums.com>
2025-09-30 08:36:50 -0600

    feat: add CookieConsentInfo to state

--- src/state/cookie-consent-state.d.ts
+++ src/state/cookie-consent-state.d.ts
@@ -1,7 +1,9 @@
 import type {CookieConsentRecord} from "chums-types";
+import type {CookieConsentInfo} from "chums-types/src/cookie-consent";
 
 export interface CookieConsentState {
     status: 'idle' | 'loading' | 'saving' | 'rejected';
     record: CookieConsentRecord | null;
     dismissed: boolean;
+    details: CookieConsentInfo|null;
 }
